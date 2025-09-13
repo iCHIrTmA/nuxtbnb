7b48f703f12300fb4596de2fb8b0a1f64c6bbd8d
iCHIrTmA <jonrue.dev@gmail.com>
2025-09-13 19:18:41 +0800

    feat: google signin and auth plugin

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,15 +15,24 @@
             <div class="app-user-menu">
                 <img src="/images/icons/house.svg" />
                 <div class="name">Host</div>
-                <script src="https://accounts.google.com/gsi/client" async defer></script>
-                <div id="googleButton">
+                <!-- <script src="https://accounts.google.com/gsi/client" async defer></script> -->
+                <div id="googleButton" class="ml-8">
                     <div
                         id="g_id_onload"
                         :data-client_id="$config.auth.clientId"
-                        data-callback="auth">
+                        data-auto_select="true"
+                        data-callback="auth"
+                    >
                     </div>
-                    
-                    <div class="g_id_signin" data-type="standard"></div>
+                    <div
+                        class="g_id_signin"
+                        data-type="icon"
+                        data-shape="circle"
+                        data-theme="outline"
+                        data-text="signin_with"
+                        data-size="large"
+                    >
+                    </div>
                 </div>
 
                 <!-- <img src="/images/user.jpg" class="avatar"/> -->
@@ -36,10 +45,6 @@
 export default {
     mounted() {
         this.$maps.makeAutoComplete(this.$refs.citySearch);
-        window.google.accounts.id.initialize({
-            client_id: this.googleClientId,
-            callback: this.handleCredentialResponse, // Reference the method
-        });
     },
     methods: {
         changed(event) {
--- plugins/auth.client.js
+++ plugins/auth.client.js
@@ -1,17 +1,40 @@
-// import Cookie from "js-cookie"
+import Cookie from "js-cookie"
 import jwt_decode from "jwt-decode"
 
 export default ({ $config }, inject) => {
-    console.log('hello');
     window.auth = (response) => {
         try {
             const token = response.credential
-            console.log(token)
             const user = jwt_decode(token)
             console.log(user)
-        } catch (e) {
-            console.error(e)
-        }
+            // store.commit('auth/user', {
+            //     fullName: user.name,
+            //     profileUrl: user.picture,
+            // });
 
+            Cookie.set($config.auth.cookieName, token, {
+                expires: 1/24,
+                sameSite: 'lax',
+            });
+        } catch (e) {
+            console.error(`JWT error ${e}`)
+        }
+    };
+
+    addScript();
+    inject('auth', {
+        signOut,
+    });
+
+    function addScript() {
+        const script = document.createElement('script');
+        script.src = "https://accounts.google.com/gsi/client"
+        script.async = true
+        document.head.appendChild(script)
+    }
+
+    function signOut() {
+        Cookie.remove($config.auth.cookieName);
+        // store.commit('auth/user', null)
     }
 }
